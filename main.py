203c6d2b64b64ae54dad7957892ec6bb4785235f
Tarandeep Mittal <33363377+tdmittens@users.noreply.github.com>
2022-07-30 21:36:34 -0400

    pandas export of json for top songs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import requests
+import pandas as pd
 from tokens import client_id, client_secret_id, user_id
 from refresh import auth_token
 
@@ -32,7 +33,7 @@ class LikedSongs:
         print(r_json)
 
     def get_top_items(self):
-        limit = 100
+        limit = 50
         time_range = "short_term"
         query = f"https://api.spotify.com/v1/me/top/tracks?limit={limit}&time_range={time_range}&offset=0"
 
@@ -42,7 +43,11 @@ class LikedSongs:
                              'Authorization': f"Bearer {self.auth_token}",
                          })
         r_json = r.json()
-        print(r_json)
+        #print(r_json)
+
+        df = pd.json_normalize(r_json[list(r_json.keys())[0]])
+        df.to_csv("top_items.csv", encoding='utf-8', index=False)
+        return r_json
 
     def get_user(self):
         query = f"https://api.spotify.com/v1/me/"
@@ -57,4 +62,4 @@ class LikedSongs:
 
 
 test = LikedSongs()
-test.get_saved()
+test_json = test.get_top_items()
